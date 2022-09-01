04754c54d34233a89c7c986e9268ab53169d3e0d
ShinshinRenma <ryan.andrew.layman@gmail.com>
2022-08-31 22:55:40 -1000

    Initial commit

---
+++ intro.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 31 16:36:32 2022
+
+@author: rlaym_000
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+t=np.arange(0.,4.,0.1)
+plt.plot(t,t,t,t+2,t,t**2)
+plt.show() # This statement is not necessary in Spyder
+# I am a picture, I am a picture, I am a picture.
